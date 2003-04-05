540482421da6dcc3222a2f5ef94ac00c9246080a
Sebastian Gutsfeld <segoh@gmx.net>
2003-04-05 17:01:50 +0000

    * nInvaders.h: added constants for alien types needed by 	nInvaders.c (doScoring), fixed declaration of doScoring, fixed 	Indentation

--- nInvaders.h
+++ nInvaders.h
@@ -8,28 +8,32 @@
 #ifndef NINVADERS
 #define NINVADERS
 
+#define BLUE_ALIEN_TYPE  0 
+#define GREEN_ALIEN_TYPE 1
+#define RED_ALIEN_TYPE   2
+#define UFO_ALIEN_TYPE   3
 
-	int main(int argv, char **argc);	// hey! it has to start somewhere!
-	//static void finish(int sig);
-	extern void render(void);
+int main(int argv, char **argc);	// hey! it has to start somewhere!
+//static void finish(int sig);
+extern void render(void);
 	
-	void game_over(int a);
-	void drawscore();
+void game_over(int a);
+void drawscore();
 	
-	int hit_bunker_test(int shotx, int shoty);
-	int hit_alien_test(int shotx, int shoty, int alienx, int alieny);
+int hit_bunker_test(int shotx, int shoty);
+int hit_alien_test(int shotx, int shoty, int alienx, int alieny);
 	
-	void doScoring();
+void doScoring(int alienType);
+
+int weite;
+int level;
+int skill_level;
+long score;
 	
-	int weite;
-	int level;
-	int skill_level;
-	long score;
-	
-	// included from globals.h
-	extern void doSleep();
-	extern void showUsage();
-	extern void showVersion();
-	extern void showGpl();
+// included from globals.h
+extern void doSleep();
+extern void showUsage();
+extern void showVersion();
+extern void showGpl();
 
 #endif
