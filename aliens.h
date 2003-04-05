b28db74990e23c8a015c33411773256edb36da19
Sebastian Gutsfeld <segoh@gmx.net>
2003-04-05 16:57:32 +0000

    * aliens.h: fixed indentation

--- aliens.h
+++ aliens.h
@@ -5,54 +5,51 @@
 #define ALIENS
 
 	
-
-		
-	struct structAliens {
-		int posX;	  // horizontal position of aliens
-		int posY;	  // vertical position of aliens
-		int right;
-		int left;
-		int bottom;
-		int speed;	  // 0: no movement; 1: one per turn; etc.
-		//int missileFired; // 0: missile not running; 1: missile running
-		//int missileX;	  // horizontal position of missile
-		//int missileY;	  // vertical position of missile
-	};
+struct structAliens {
+	int posX;	  // horizontal position of aliens
+	int posY;	  // vertical position of aliens
+	int right;
+	int left;
+	int bottom;
+	int speed;	  // 0: no movement; 1: one per turn; etc.
+	//int missileFired; // 0: missile not running; 1: missile running
+	//int missileX;	  // horizontal position of missile
+	//int missileY;	  // vertical position of missile
+};
 	
-	struct structAliens a;
+struct structAliens a;
 	
 
-	int shootme_counter;	
-	int shipnum;	
+int shootme_counter;	
+int shipnum;	
 
-	#define ALIENS_MAX_NUMBER_X 10
-	#define ALIENS_MAX_NUMBER_Y 5	
-	#define ALIENS_MAX_MISSILES 50	
-	int lowest_ship[ALIENS_MAX_NUMBER_X];
-	int alienshotx[ALIENS_MAX_MISSILES];
-	int alienshoty[ALIENS_MAX_MISSILES];
-	int alienshotnum;
+#define ALIENS_MAX_NUMBER_X 10
+#define ALIENS_MAX_NUMBER_Y 5	
+#define ALIENS_MAX_MISSILES 50	
 
+int lowest_ship[ALIENS_MAX_NUMBER_X];
+int alienshotx[ALIENS_MAX_MISSILES];
+int alienshoty[ALIENS_MAX_MISSILES];
+int alienshotnum;
 	
-	void aliensReset();
-	void bunkersReset();
+void aliensReset();
+void bunkersReset();
 	
-	int aliensMove();
-	int aliensMissileMove();
+int aliensMove();
+int aliensMissileMove();
 
-	int bunker[BUNKERHEIGHT][BUNKERWIDTH + 1];	
+int bunker[BUNKERHEIGHT][BUNKERWIDTH + 1];	
 	
-	void render();
+void render();
 	
-	// methods that handle graphic display, from view.c
-	extern void aliensDisplay(int x, int y, int wid, int hgt);
-	extern void aliensClear(int x, int y, int wid, int hgt);
-	extern void aliensRefresh(int level);
-	extern void aliensMissileDisplay(int x, int y);
-	extern void aliensMissileClear(int x, int y);
-	extern void bunkersClearElement(int x, int y);
-	extern void bunkersClear();
-	extern void bunkersRefresh();
-	
+// methods that handle graphic display, from view.c
+extern void aliensDisplay(int x, int y, int wid, int hgt);
+extern void aliensClear(int x, int y, int wid, int hgt);
+extern void aliensRefresh(int level);
+extern void aliensMissileDisplay(int x, int y);
+extern void aliensMissileClear(int x, int y);
+extern void bunkersClearElement(int x, int y);
+extern void bunkersClear();
+extern void bunkersRefresh();
 	
 #endif
