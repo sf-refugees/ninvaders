b36acad2cdbe1e64d28be4d23a628783cee55258
Sebastian Gutsfeld <segoh@gmx.net>
2003-04-05 17:04:33 +0000

    * view.h: fixed indentation, added constans UFOWIDTH and UFOPOSY, added 	declarations of ufoDisplay, ufoRefresh and ufoClear

--- view.h
+++ view.h
@@ -4,44 +4,50 @@
 
 #ifndef VIEW
 #define VIEW
-	#include "globals.h"
 
-	#define SCREENHEIGHT 24
-	#define SCREENWIDTH 80	
-	
-	#define PLAYERWIDTH 5
-	#define PLAYERPOSY (SCREENHEIGHT-2)
+#include "globals.h"
 
-	#define BUNKERWIDTH 80
-	#define BUNKERHEIGHT 4
-	#define BUNKERX 0
-	#define BUNKERY (SCREENHEIGHT-8)
+#define SCREENHEIGHT 24
+#define SCREENWIDTH 80	
 	
-	#define ALIENS_MAX_NUMBER_X 10
-	#define ALIENS_MAX_NUMBER_Y 5	
-	#define ALIENS_MAX_MISSILES 50
-	
+#define PLAYERWIDTH 5
+#define PLAYERPOSY (SCREENHEIGHT-2)
 
-	int alienBlock[ALIENS_MAX_NUMBER_Y][ALIENS_MAX_NUMBER_X];	
-
+#define BUNKERWIDTH 80
+#define BUNKERHEIGHT 4
+#define BUNKERX 0
+#define BUNKERY (SCREENHEIGHT-8)
 	
-	extern void graphicEngineInit();
-	
-	extern void playerDisplay(int x, int y);
-	extern void playerClear(int x, int y);
-	extern void playerMissileDisplay(int x, int y);
-	extern void playerMissileClear(int x, int y);
-	extern void playerExplode(int x, int y);
-	extern void aliensDisplay(int x, int y, int wid, int hgt);
-	extern void aliensClear(int x, int y, int wid, int hgt);
-	extern void aliensRefresh(int level);
-	extern void aliensMissileDisplay(int x, int y);
-	extern void aliensMissileClear(int x, int y);
-	extern void bunkersDisplay(int *pBunker);
-	extern void bunkersClear();
-	extern void bunkersClearElement(int x, int y);
-	extern void gameOverDisplay(int x, int y);
+#define ALIENS_MAX_NUMBER_X 10
+#define ALIENS_MAX_NUMBER_Y 5	
+#define ALIENS_MAX_MISSILES 50
 
-	extern void statusDisplay(int level, int score, int lives);	
-	extern void refreshScreen();
+#define UFOWIDTH 5
+#define UFOPOSY 0
+
+int alienBlock[ALIENS_MAX_NUMBER_Y][ALIENS_MAX_NUMBER_X];	
+	
+void graphicEngineInit();
+	
+void playerDisplay(int x, int y);
+void playerClear(int x, int y);
+void playerMissileDisplay(int x, int y);
+void playerMissileClear(int x, int y);
+void playerExplode(int x, int y);
+void aliensDisplay(int x, int y, int wid, int hgt);
+void aliensClear(int x, int y, int wid, int hgt);
+void aliensRefresh(int level);
+void aliensMissileDisplay(int x, int y);
+void aliensMissileClear(int x, int y);
+void bunkersDisplay(int *pBunker);
+void bunkersClear();
+void bunkersClearElement(int x, int y);
+void ufoDisplay(int x, int y);
+void ufoRefresh();
+void ufoClear(int x, int y);
+void gameOverDisplay(int x, int y);
+
+void statusDisplay(int level, int score, int lives);	
+void refreshScreen();
+
 #endif
